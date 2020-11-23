821d7ec31a36bd40459e86450690605e723d3e16
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-23 19:04:05 +0000

    Create ArrayList, checked size, removed items.

--- Playlist.java
+++ Playlist.java
@@ -2,6 +2,21 @@ import java.util.ArrayList;
 
 public class Playlist {
     public static void main(String[] args) {
+        ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
 
+        desertIslandPlaylist.add("Bad Kingdom");
+        desertIslandPlaylist.add("Mad Doctor");
+        desertIslandPlaylist.add("False Idols");
+        desertIslandPlaylist.add("Supersyling");
+        desertIslandPlaylist.add("Get down");
+        desertIslandPlaylist.add("Paradise Circus");
+
+        System.out.println(desertIslandPlaylist);
+
+        int arrSize = desertIslandPlaylist.size();
+        System.out.println("Array size is: " + arrSize + "\n");
+
+        desertIslandPlaylist.remove(5);
+        System.out.println("Array size after cut is: " + desertIslandPlaylist.size() + "\n");
     }
 }
